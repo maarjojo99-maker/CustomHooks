41a31dbe7cef094d6d71c6ddd5bafb1d471af12a
Mohsin Abbas <maarjojo99@gmail.com>
2025-12-23 12:54:52 +0500

    Initial commit (no node_modules)

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Fragment } from 'react/jsx-runtime';
+import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import { useSelector } from 'react-redux';  
+function App() {
+  const auth = useSelector(state => state.auth.isAuthenticated); 
+
+  return (
+    <Fragment>
+      <Header/>
+      {!auth && <Auth />}
+      {auth && <UserProfile />}
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
